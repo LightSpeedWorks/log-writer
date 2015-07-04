d8c65b7cb9a30c4fc8169d2c82b2f9227057f921
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-04 14:07:53 +0900

    example script

--- examples/log-writer-ex.js
+++ examples/log-writer-ex.js
@@ -7,11 +7,12 @@ var LogWriter = require('../log-writer');
 LogWriterTest_test();
 
 function LogWriterTest_test() {
-  var writer = new LogWriter('log-writer-test-%s.log');
+  var writer = new LogWriter('log-writer-ex-%s.log');
   writer.write('log-writer-test write\r\n');
   writer.writeln('log-writer-test writeln');
   writer.write('log-writer-test write\r\n');
   writer.writeln('log-writer-test writeln');
+  writer.writeln({x:1, y:2});
   writer.writeln();
   writer.end();
 }
