51b0361948e6a6e4e4297801d6299b3329e15e04
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-07-09 15:14:26 +0900

    revise example

--- examples/log-writer-ex.js
+++ examples/log-writer-ex.js
@@ -13,6 +13,22 @@ function LogWriterTest_test() {
   writer.write('log-writer-test write\r\n');
   writer.writeln('log-writer-test writeln');
   writer.writeln({x:1, y:2});
+  writer.writeln([1, 2, 3]);
+  writer.writeln(123, 12.34);
+  writer.writeln(true, false);
+  writer.writeln('hello', "world");
+  writer.writeln(null, undefined);
+  writer.write({x:1, y:2});
+  writer.writeln();
+  writer.write([1, 2, 3]);
+  writer.writeln();
+  writer.write(123, 12.34);
+  writer.writeln();
+  writer.write(true, false);
+  writer.writeln();
+  writer.write('hello', "world");
+  writer.writeln();
+  writer.write(null, undefined);
   writer.writeln();
   writer.end();
 }
